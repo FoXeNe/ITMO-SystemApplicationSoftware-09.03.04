222e38db9ce2aae69c81b261bde23931cbbf5fbc
FoXeNe <121698405+FoXeNe@users.noreply.github.com>
2025-09-21 23:32:14 +0300

    Add files via upload

---
+++ 1st year/opd/lab1/src/task3.sh
@@ -0,0 +1,8 @@
+cd lab0
+ln roserade5 porygon20/mantykeroserade
+ln -s roserade5 cleffa5/pidoveroserade
+cp vanilluxe0 cleffa5/bellsproutvanilluxe
+cp -r cleffa5 vigoroth5/musharna/
+ln -s cleffa5 Copy_32
+cp roserade5 porygon20/sandshrew
+cat cleffa5/servine porygon20/mantyke > roserade5_90
