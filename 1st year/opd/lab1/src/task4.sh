d3d37bc2a4f203404ab6a4404b10039d8d34a281
FoXeNe <121698405+FoXeNe@users.noreply.github.com>
2025-09-24 23:59:17 +0300

    Update task4.sh

--- 1st year/opd/lab1/src/task4.sh
+++ 1st year/opd/lab1/src/task4.sh
@@ -2,7 +2,7 @@ cd lab0
 cat roserade5 | wc -m > /tmp/count
 ls -R cleffa5 2>/tmp/errors | sort 
 cd ..
-ls -R lab0 2>/dev/null | grep "^t" | sort -r
+ls -R lab0 2>/dev/null | grep '^t' | sort -r
 cd lab0
-cat -n cleffa5/bellsprout cleffa5/pidove cleffa5/servine porygon20/mantyke porygon20/alakazam porygon20/gligar 2>/dev/null | sort -r
-ls cleffa5/bellsprout cleffa5/pidove cleffa5/servine porygon20/mantyke porygon20/alakazam porygon20/gligar 2>/tmp/errors | grep -i "e$"
+ls -l cleffa5/bellsprout cleffa5/pidove cleffa5/servine porygon20/mantyke porygon20/alakazam porygon20/gligar 2>/dev/null | sort -k7r
+cat cleffa5/bellsprout cleffa5/pidove cleffa5/servine porygon20/mantyke porygon20/alakazam porygon20/gligar 2>/tmp/errors | grep -i 'e$'
