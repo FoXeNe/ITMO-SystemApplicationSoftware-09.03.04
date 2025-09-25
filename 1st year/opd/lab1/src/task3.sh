d170aac03db6c44bf87ada56b390813b9a2b45b8
FoXeNe <121698405+FoXeNe@users.noreply.github.com>
2025-09-25 17:26:17 +0300

    Update task3.sh

--- 1st year/opd/lab1/src/task3.sh
+++ 1st year/opd/lab1/src/task3.sh
@@ -1,8 +1,13 @@
 cd lab0
+chmod u+w porygon20
+chmod u+rw vanilluxe0
+chmod u+rw cleffa5
+chmod u+r cleffa5/bellsprout
+chmod u+r cleffa5/charmander
 ln roserade5 porygon20/mantykeroserade
 ln -s roserade5 cleffa5/pidoveroserade
 cp vanilluxe0 cleffa5/bellsproutvanilluxe
-cp -r cleffa5 vigoroth5/musharna/
+cp -R cleffa5 vigoroth5/musharna/
 ln -s cleffa5 Copy_32
 cp roserade5 porygon20/sandshrew
-cat cleffa5/servine porygon20/mantyke > roserade5_90
+cat cleffa5/servine porygon20/mantyke > roserade5_90g
